5e1d976ac6cea04759069ef0328bb13e2f354d64
ulabrezina <ulabrezina@gmail.com>
2024-09-14 20:50:06 +0500

    Task1

---
+++ Tyuiu.BrezinaYS.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tyuiu.BrezinaYS.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            Console.ReadKey();
+            string name = "Юлия";
+            int age = 17;
+            Console.WriteLine("Здравствуйте. Меня зовут " + name + " мне " + age + "лет!");
+        }
+    }
+}
