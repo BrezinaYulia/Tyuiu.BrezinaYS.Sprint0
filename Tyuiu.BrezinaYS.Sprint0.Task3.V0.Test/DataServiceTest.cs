868e3cf665c761a95f9c607992ba5660103e22dc
ulabrezina <ulabrezina@gmail.com>
2024-09-16 22:38:58 +0500

    Task3

---
+++ Tyuiu.BrezinaYS.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Tyuiu.BrezinaYS.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.BrezinaYS.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
